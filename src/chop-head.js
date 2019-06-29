d4190fceeeaf66bdbca25f7f1a6a7fbe2b141d22
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-06-29 20:15:17 +0530

    Extracted functions

--- src/chop-head.js
+++ src/chop-head.js
@@ -1,20 +1,58 @@
-const chopHead = head => {
-  if (head.length === 0) return [];
-  if (head[0] === 1 && head.length === 1) return [];
-  const result = new Array(head[0] - 1).fill(head[0] - 1);
-  head.shift();
-  result.push(...head);
-  return result;
+const isSingleLevelOneHead = heads => {
+  return heads[0] === 1 && heads.length === 1;
 };
 
-const repetedlyChopHead = heads => {
-  const extendedHeads = [heads.slice(0)];
-  while (extendedHeads[extendedHeads.length - 1].length !== 0) {
-    extendedHeads.push(
-      chopHead(extendedHeads[extendedHeads.length - 1].slice(0))
-    );
+const isDead = heads => {
+  return heads.length === 0;
+};
+
+const getLevel = heads => {
+  return heads[0] - 1;
+};
+
+const spawnHeads = level => {
+  return new Array(level).fill(level);
+};
+
+const clone = heads => {
+  return heads.slice(0);
+};
+
+const cutFirstHead = heads => {
+  const headsClone = clone(heads);
+  headsClone.shift();
+  return headsClone;
+};
+
+const getLastHead = heads => {
+  return heads[heads.length - 1];
+};
+
+const addHeads = (heads, additionalHeads) => {
+  const headsClone = clone(heads);
+  const additionalHeadsClone = clone(additionalHeads);
+  headsClone.push(...additionalHeadsClone);
+  return headsClone;
+};
+
+const chopHead = heads => {
+  const headsClone = clone(heads);
+  if (isDead(headsClone)) return [];
+  if (isSingleLevelOneHead(headsClone)) return [];
+
+  const level = getLevel(headsClone);
+  const spawnedHeads = spawnHeads(level);
+  const shiftedHeads = cutFirstHead(headsClone);
+
+  return addHeads(spawnedHeads, shiftedHeads);
+};
+
+const chopHeadTillAlive = heads => {
+  const extendedHeads = [clone(heads)];
+  while (!isDead(getLastHead(extendedHeads))) {
+    extendedHeads.push(chopHead(getLastHead(extendedHeads)));
   }
   return extendedHeads;
 };
 
-console.log(repetedlyChopHead([3]));
+export default chopHeadTillAlive;
